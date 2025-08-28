1f0e33f8f0dd953046bd9d1276cccfc7b2a9d9ca
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-08-28 10:12:10 -0400

    added stuff

---
+++ pointclouds/sheridan/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 6050469,
+    "boundingBox": {
+        "lx": -11.415582656860352,
+        "ly": -10.409562110900879,
+        "lz": -0.295,
+        "ux": 20.29541734313965,
+        "uy": 21.301437889099124,
+        "uz": 31.416
+    },
+    "tightBoundingBox": {
+        "lx": -11.415582656860352,
+        "ly": -10.409562110900879,
+        "lz": -0.295,
+        "ux": 20.29541734313965,
+        "uy": 1.3184378890991209,
+        "uz": 26.817
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.274625301361084,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
