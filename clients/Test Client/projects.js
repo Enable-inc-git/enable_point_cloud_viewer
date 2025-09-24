fe80c8672b09cbbc8d0ca4cade37e6b34138f015
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-09-24 06:54:20 -0400

    bug fix

---
+++ clients/Test Client/projects.js
@@ -0,0 +1,6 @@
+// Projects for THIS client only
+window.PROJECTS = {
+  sheridan: { name: "Sheridan" } // folder name under /pointclouds/
+  // add more later like:
+  // east_wing: { name: "East Wing" }
+};
