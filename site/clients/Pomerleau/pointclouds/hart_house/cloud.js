15e01091d5beea251d8cd4804573d70a4638f1f3
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-10-15 18:33:19 -0400

    harthouse

---
+++ site/clients/Pomerleau/pointclouds/hart_house/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2188376,
+    "boundingBox": {
+        "lx": -23.621496200561525,
+        "ly": -12.512439727783204,
+        "lz": -0.358,
+        "ux": 18.06950379943848,
+        "uy": 29.1785602722168,
+        "uz": 41.333000000000009
+    },
+    "tightBoundingBox": {
+        "lx": -23.621496200561525,
+        "ly": -12.512439727783204,
+        "lz": -0.358,
+        "ux": 18.06950379943848,
+        "uy": 7.738560272216798,
+        "uz": 17.115000000000003
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 0.3610546588897705,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
