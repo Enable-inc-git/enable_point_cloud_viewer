3e84b1c3e08f31328f725cbee2030b86c1b53058
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-11-04 04:39:03 -0500

    added hole13

---
+++ site/clients/Fida Restoration/Royal Ontario Golf Course Hole 13/pointclouds/hole13/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "EPSG:26917",
+    "points": 5608404,
+    "boundingBox": {
+        "lx": -0.1294684112071991,
+        "ly": 3.2537930011749269,
+        "lz": -3.7880000000000004,
+        "ux": 29.6915315887928,
+        "uy": 33.074793001174928,
+        "uz": 26.032999999999999
+    },
+    "tightBoundingBox": {
+        "lx": -0.1294684112071991,
+        "ly": 3.2537930011749269,
+        "lz": -3.7880000000000004,
+        "ux": 23.3595315887928,
+        "uy": 33.074793001174928,
+        "uz": 6.543
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.2582574486732483,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
