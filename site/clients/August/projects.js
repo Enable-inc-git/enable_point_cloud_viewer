f64f27f272d40a2b21d1835ae104cf0a59a53aeb
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-11-06 15:13:06 -0500

    revised js file

--- site/clients/August/projects.js
+++ site/clients/August/projects.js
@@ -1,6 +1,6 @@
 window.PROJECTS = {
   "hole13": {
-    name: "Hole 13",
-    folder: "Royal Ontario Golf Course Hole 13/pointclouds/hole13"
+    name: "hydrovault",
+    folder: "tomken_shoring/pointclouds/hydrovault"
   }
 };
