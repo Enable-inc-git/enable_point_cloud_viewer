0519cde54fd154cf6a5412efa7d22e602ee7024f
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-11-06 14:59:58 -0500

    august tomken shoring added

---
+++ site/clients/August/tomken_shoring/pointclouds/hydrovault/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "EPSG:26917",
+    "points": 32665758,
+    "boundingBox": {
+        "lx": -5.331014156341553,
+        "ly": -5.414653778076172,
+        "lz": -0.078,
+        "ux": 9.895985843658448,
+        "uy": 9.812346221923829,
+        "uz": 15.149000000000001
+    },
+    "tightBoundingBox": {
+        "lx": -5.331014156341553,
+        "ly": -5.414653778076172,
+        "lz": -0.078,
+        "ux": 4.809985843658447,
+        "uy": 9.812346221923829,
+        "uz": 8.63
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.13186968863010407,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
