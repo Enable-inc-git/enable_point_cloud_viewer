cb1fc8e517c94478d5d96469231d0362582ee9a5
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-11-18 11:36:16 -0500

    update to .js file

--- site/clients/WJ Properties/projects.js
+++ site/clients/WJ Properties/projects.js
@@ -1,6 +1,6 @@
 window.PROJECTS = {
-  "hole13": {
-    name: "Hole 13",
-    folder: "Royal Ontario Golf Course Hole 13/pointclouds/hole13"
+  "ph8": {
+    name: "PH8",
+    folder: "140 Carlton/pointclouds/ph8"
   }
 };
