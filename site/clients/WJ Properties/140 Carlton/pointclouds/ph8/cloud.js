dada46a3dddacf2a7d6261eae7931e04e521834a
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-11-18 11:29:37 -0500

    add wj 140 carlton

---
+++ site/clients/WJ Properties/140 Carlton/pointclouds/ph8/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "EPSG:26917",
+    "points": 3819884,
+    "boundingBox": {
+        "lx": -7.615471363067627,
+        "ly": 0.6423544883728027,
+        "lz": -3.017,
+        "ux": -0.405471363067627,
+        "uy": 7.852354488372803,
+        "uz": 4.193
+    },
+    "tightBoundingBox": {
+        "lx": -7.615471363067627,
+        "ly": 0.6423544883728027,
+        "lz": -3.017,
+        "ux": -0.405471363067627,
+        "uy": 5.694354488372803,
+        "uz": 1.6320000000000002
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.062440432608127597,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
---
+++ site/clients/WJ Properties/projects.js
@@ -0,0 +1,6 @@
+window.PROJECTS = {
+  "hole13": {
+    name: "Hole 13",
+    folder: "Royal Ontario Golf Course Hole 13/pointclouds/hole13"
+  }
+};
