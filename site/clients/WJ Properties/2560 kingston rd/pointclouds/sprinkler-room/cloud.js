26d5c78211d4830e21716aa43e56b6a582d849d0
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-12-16 18:34:51 -0500

    added kingston rd sprinkler rm

---
+++ site/clients/WJ Properties/2560 kingston rd/pointclouds/sprinkler-room/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "EPSG:26917",
+    "points": 24306953,
+    "boundingBox": {
+        "lx": -9.318965911865235,
+        "ly": -14.754410743713379,
+        "lz": -0.588,
+        "ux": 16.450034088134769,
+        "uy": 11.014589256286623,
+        "uz": 25.181
+    },
+    "tightBoundingBox": {
+        "lx": -9.318965911865235,
+        "ly": -14.754410743713379,
+        "lz": -0.588,
+        "ux": 10.651034088134765,
+        "uy": 11.014589256286623,
+        "uz": 8.02
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.2231660932302475,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
