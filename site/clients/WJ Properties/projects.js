3754d971ea28e23f2240f2769c3099cc3530c1de
kevincousins <88157999+kevincousins@users.noreply.github.com>
2025-12-16 19:13:46 -0500

    updates projects.js

--- site/clients/WJ Properties/projects.js
+++ site/clients/WJ Properties/projects.js
@@ -2,5 +2,9 @@ window.PROJECTS = {
   "ph8": {
     name: "PH8",
     folder: "140 Carlton/pointclouds/ph8"
+  },
+  "sprinkler_room": {
+    name: "2560 Kingston Rd - Sprinkler Room",
+    folder: "2560 kingston rd/pointclouds/sprinkler-room"
   }
 };
