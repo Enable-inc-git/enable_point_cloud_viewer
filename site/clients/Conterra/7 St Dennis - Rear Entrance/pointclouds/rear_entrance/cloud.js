5090f9d3d251da6aa21c3412c5f5391168b5dc61
kevincousins <88157999+kevincousins@users.noreply.github.com>
2026-01-30 16:36:12 -0500

    added 7 st dennis rear entry

---
+++ site/clients/Conterra/7 St Dennis - Rear Entrance/pointclouds/rear_entrance/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "EPSG:26917",
+    "points": 6533335,
+    "boundingBox": {
+        "lx": -7.883183002471924,
+        "ly": -4.562152862548828,
+        "lz": -2.657,
+        "ux": 0.36881699752807686,
+        "uy": 3.6898471374511727,
+        "uz": 5.595000000000001
+    },
+    "tightBoundingBox": {
+        "lx": -7.883183002471924,
+        "ly": -4.562152862548828,
+        "lz": -2.657,
+        "ux": -2.506183002471924,
+        "uy": 3.6898471374511727,
+        "uz": 3.803
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.0714644193649292,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
---
+++ site/clients/Conterra/projects.js
@@ -0,0 +1,6 @@
+window.PROJECTS = {
+  "st_dennis_rear": {
+    name: "7 St Dennis - Rear Entrance",
+    folder: "7 St Dennis - Rear Entrance/pointclouds/rear_entrance"
+  }
+};
