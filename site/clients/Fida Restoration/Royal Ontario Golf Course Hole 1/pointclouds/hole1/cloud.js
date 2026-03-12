bb310f630f1391e1a43700f51c172c9f6d25b23d
kevincousins <88157999+kevincousins@users.noreply.github.com>
2026-03-12 16:48:10 -0400

    added royal ontario golf course hole 1

---
+++ site/clients/Fida Restoration/Royal Ontario Golf Course Hole 1/pointclouds/hole1/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "EPSG:26917",
+    "points": 4229856,
+    "boundingBox": {
+        "lx": -11.40873908996582,
+        "ly": -24.025989532470704,
+        "lz": 0.48,
+        "ux": 11.02226091003418,
+        "uy": -1.5949895324707022,
+        "uz": 22.911
+    },
+    "tightBoundingBox": {
+        "lx": -11.40873908996582,
+        "ly": -24.025989532470704,
+        "lz": 0.48,
+        "ux": 9.26626091003418,
+        "uy": -1.5949895324707022,
+        "uz": 4.3790000000000008
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.19425815343856812,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
--- site/clients/Fida Restoration/projects.js
+++ site/clients/Fida Restoration/projects.js
@@ -2,5 +2,11 @@ window.PROJECTS = {
   "hole13": {
     name: "Hole 13",
     folder: "Royal Ontario Golf Course Hole 13/pointclouds/hole13"
+  },
+
+  "hole1": {
+    name: "Hole 1",
+    folder: "Royal Ontario Golf Course Hole 1/pointclouds/hole1"
   }
+
 };
