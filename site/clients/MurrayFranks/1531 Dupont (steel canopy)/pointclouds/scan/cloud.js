374ae7c7b48bf418fe27a22e523c4819261fca8e
kevincousins <88157999+kevincousins@users.noreply.github.com>
2026-06-18 13:07:45 -0400

    Dupont octree (batch 1)

---
+++ site/clients/MurrayFranks/1531 Dupont (steel canopy)/pointclouds/scan/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 9215610,
+    "boundingBox": {
+        "lx": -9.290827751159668,
+        "ly": -26.919082641601564,
+        "lz": -0.545,
+        "ux": 27.24317224884033,
+        "uy": 9.614917358398437,
+        "uz": 35.989
+    },
+    "tightBoundingBox": {
+        "lx": -9.290827751159668,
+        "ly": -26.919082641601564,
+        "lz": -0.545,
+        "ux": 10.194172248840332,
+        "uy": 9.614917358398437,
+        "uz": 14.252
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.31639373302459719,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
